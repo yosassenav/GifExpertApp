dc7e5f73a3f78205abcf10dfa7290c8dedb32926
yosassenav <mromerovanessa.soy@gmail.com>
2024-02-02 08:55:12 -0500

    adding two new test scenarios: component must show image with src and alt and component must show title

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas en <GifItem/>',()=>{
@@ -8,7 +8,20 @@ describe('Pruebas en <GifItem/>',()=>{
     test('debe hacer match con el snapshot',()=>{
         const { container } = render(<GifItem title={title} url={url}/>);
         expect(container).toMatchSnapshot();
+    });
 
-    })
+    test('debe mostrar la imagen con el url indicado y alt',()=>{
+        render(<GifItem title={title} url={url}/>);
+        // screen.debug();
+        const {src, alt} =  screen.getByRole("img");
+        expect(src).toBe(url);
+        expect(alt).toBe(alt);
+    });
+
+    test('debe mostrar el titulo en el componente',()=>{
+        render(<GifItem title={title} url={url}/>);
+        expect(screen.getByText(title)).toBeTruthy();   
+        
+    });
 
 });
